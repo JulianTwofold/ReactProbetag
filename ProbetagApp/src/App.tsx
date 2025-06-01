a41162b01ac654d0055802fc626ae965fb94d699
Julian Bachmann <julian.bachmann@bluewin.ch>
2025-06-01 19:12:46 +0200

    Code funktioniert ist aber hässlich. Nutzer Kann nun Anfragen mit Id machen.

--- ProbetagApp/src/App.tsx
+++ ProbetagApp/src/App.tsx
@@ -6,7 +6,9 @@ function App(){
     <strong>API Anfrage Ohne Angabe:</strong><br/><br/>
     <FetchApi /><br/><br/>
     <strong>API Anfrage mit Id:</strong><br/><br/>
-    <FetchApi userId={4}/><br/><br/>
+    <FetchApi userId={1}/><br/><br/>
+    <strong>API Anfrage um alle User zu holen:</strong><br/><br/>
+    <FetchApi userId={0}/><br/><br/>
     </div>
 }
 
--- ProbetagApp/src/components/FetchApi.tsx
+++ ProbetagApp/src/components/FetchApi.tsx
@@ -14,7 +14,8 @@ interface Props {
 
 const FetchApi = ({ userId }: Props) => {
   const [persons, setPersons] = useState<User[] | null>(null);
-  const url = `/api/users?id=${userId}`;
+  //API Doesnt work. Only id=1 shows 1 user (https://jsonplaceholder.org/users?id=1)
+  let url = `/api/users?id=${userId}`;
 
   const getFallbackData = async (): Promise<User[]> => {
     const response = await fetch("/users.json");
@@ -24,12 +25,16 @@ const FetchApi = ({ userId }: Props) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        if (userId === 0) {
+          url = "/api/users";
+          userId = -1;
+        }
         if (!userId) {
+          console.log("Falllllllllllllllllbackkkkkkkkkkkkkkkk");
           const fallbackData = await getFallbackData();
           setPersons(fallbackData);
           return;
         }
-
         const response = await fetch(url);
         if (!response.ok) {
           const fallbackData = await getFallbackData();
@@ -38,7 +43,12 @@ const FetchApi = ({ userId }: Props) => {
         }
 
         const data = await response.json();
-        setPersons(data);
+        if (userId === -1) {
+          setPersons(data);
+        }
+        else{
+        setPersons([data]);
+        }
       } catch {
         const fallbackData = await getFallbackData();
         setPersons(fallbackData);
